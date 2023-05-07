cfe78ab82d3f2831c0dbfff9bbb093d3d492ea03
ishmam-br10 <ishmam.bin.rofi1@g.bracu.ac.bd>
2023-05-07 17:10:32 +0600

    new file lecture 3

---
+++ lecture3_introduction_to_circular_array.java
@@ -0,0 +1,7 @@
+public class lecture3_introduction_to_circular_array {
+    public static void main(String[] args) {
+        int[] linear = {10,20,30,40,50,0,0,0,0,0,0,0};
+        int[] circular = {0,0,0,0,0,0,0,10,20,30,40,50};
+        System.out.println(circular.length);
+    }
+}
