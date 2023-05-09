736fa2d0f52b92f8246a417f74b84f8d9bf14410
ishmam-br10 <ishmam.bin.rofi1@g.bracu.ac.bd>
2023-05-09 21:18:45 +0600

    new file:   lecture3_circular_array_traversal.java modified:   .vscode/launch.json lecture3_circular_array_traversal.py

---
+++ lecture3_circular_array_traversal.java
@@ -0,0 +1,18 @@
+public class lecture3_circular_array_traversal {
+    public static void main(String[] args) {
+        int[] circular = {40,50,0,0,0,0,0,0,10,20,30};
+        print(circular, 8, 5);
+    }
+    public static void print(int[] c, int start, int size) {
+        int index = start;
+        for(int i=0; i< size; i++){
+            System.out.println(c[index]);
+            index = (index +1)%(c.length);
+        // alternative way
+        //if(index == c.length){
+            //index = 0;
+            //this will make index zero when it reaches limit
+        //}
+        }
+    }
+}
