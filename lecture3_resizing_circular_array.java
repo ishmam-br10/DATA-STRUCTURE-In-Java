75a8bd49915e8f1a0595dda8c0b8e22b3f4b5a46
Ishmam Bin Rofi <ishmam.bin.rofi1@g.bracu.ac.bd>
2023-05-11 22:55:05 +0600

    modified:   lecture3_resizing_circular_array.java

--- lecture3_resizing_circular_array.java
+++ lecture3_resizing_circular_array.java
@@ -5,13 +5,16 @@ public class lecture3_resizing_circular_array {
         print(n, 4, 5);
 
     }
-    public static int[] resize(int[] l, int start, int size) {
-        int[] circular = new int[l.length + 2];
+    public static int[] resize(int[] c, int start, int size) {
+        int[] circular = new int[c.length + 2];
         int index_circ = start;
+        int index_resizedCirc = start;
         for(int i =0; i<size;i++){
-            circular[index_circ] = l[i];
-            index_circ = (index_circ+1)%(circular.length);
+            circular[index_resizedCirc] = c[index_circ];
+            index_resizedCirc = (index_resizedCirc+1)%(circular.length);
+            index_circ = (index_circ+1)%(c.length);
         }
+
         return circular;
     }
     public static void print(int[] c, int start, int size) {
